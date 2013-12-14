e4cd87a25bf9ecd5a0aba8d6db6dd48a9c78b4fb
crahen <crahen@nowhere.org>
2013-12-14 09:56:21 -0800

    .

--- bin/get-pool-url.sh
+++ bin/get-pool-url.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 ROOT="$(cd `dirname $0`/..; pwd)"
 # Get the placement from the instance metadata service
-ZONE=$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's,\r,,')
-REGION=$(echo ZONE | sed -e 's,\r,,' -e 's,.$,,')
+ZONE=$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone | tr -d '\r')
+REGION=$(echo ZONE | tr -d '\r' | sed -e 's,.$,,')
 ## Get the instance from the instance metadata service
-INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id | sed -e 's,\r,,')
+INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id | tr -d '\r')
 # Get the tag for the instance from the ec2 API
-TAG=$(ec2-describe-instances $INSTANCE --show-empty-fields --region $REGION | sed -e 's,\r,,' | grep TAG | grep URL)
+TAG=$(ec2-describe-instances $INSTANCE --show-empty-fields --region $REGION | tr -d '\r' | grep TAG | grep URL)
 # Get the pool URL from the tag.
 echo $TAG | sed -e 's,.*URL[^h]*,,'
