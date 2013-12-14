f6a822d81cb3d7b8f98ce03851484ab243686aa8
crahen <crahen@nowhere.org>
2013-12-14 09:44:00 -0800

    .

--- bin/get-pool-url.sh
+++ bin/get-pool-url.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 ROOT="$(cd `dirname $0`/..; pwd)"
 # Get the instance id from the instance metadata service
-INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id | sed -e 's,\r,,')
 # Get the tag for the instance from the ec2 API
-TAG=$(ec2-describe-instances $INSTANCE --show-empty-fields | grep TAG | grep URL)
+TAG=$(ec2-describe-instances $INSTANCE --show-empty-fields  | sed -e 's,\r,,' | grep TAG | grep URL)
 # Get the pool URL from the tag.
 echo $TAG | sed -e 's,.*URL[^h]*,,'
