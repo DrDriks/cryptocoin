d32913367acc094dc66ae6e62bc7277a3938fea4
crahen <crahen@nowhere.org>
2013-12-14 09:57:37 -0800

    .

--- bin/get-pool-url.sh
+++ bin/get-pool-url.sh
@@ -2,7 +2,7 @@
 ROOT="$(cd `dirname $0`/..; pwd)"
 # Get the placement from the instance metadata service
 ZONE=$(wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone | tr -d '\r')
-REGION=$(echo ZONE | tr -d '\r' | sed -e 's,.$,,')
+REGION=$(echo $ZONE | tr -d '\r' | sed -e 's,.$,,')
 ## Get the instance from the instance metadata service
 INSTANCE=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id | tr -d '\r')
 # Get the tag for the instance from the ec2 API
